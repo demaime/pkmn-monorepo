006f1b16030156618e99ca35fee32e31037d64be
demaime <aimedemian@gmail.com>
2026-03-30 15:23:38 -0300

    configurado proyecto, creado esqueleto register y login

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,23 +1,41 @@
+import { useState } from "react";
 import "./App.css";
+import RegisterInput from "./components/RegisterComponent";
+import LoginComponent from "./components/LoginComponent";
 
 function App() {
+  const [registerOrLogin, setRegisterOrLogin] = useState<string | null>(null);
+
   return (
-    <div className="min-w-screen min-h-screen bg-gray-300 font-semibold flex flex-col items-center p-2">
-      <h1>PKMN FRONT</h1>
-      <div className="flex flex-col items-center justify-center h-40 gap-4 mt-8">
-        <input
-          placeholder="user"
-          className="w-50 border rounded border-gray-700 h-12 px-2"
-          type="text"
-        />
-        <input
-          placeholder="password"
-          className="w-50 border rounded border-gray-700 h-12 px-2"
-          type="text"
-        />
-        <button className="bg-gray-900 text-gray-300 rounded-lg px-4 py-2">
-          enviar
-        </button>
+    <div className="h-screen bg-mauve-200 flex flex-col p-2 items-center justify-center righteous-regular inset-ring-4 inset-ring-mauve-300">
+      <div className="p-2 border rounded-lg border-mauve-300 flex flex-col items-center justify-center">
+        <div className="flex p-4 gap-4 w-full items-center justify-center">
+          <h1 className="text-mauve-600 text-5xl font-black">PKMN</h1>
+          <img src="/logo.png" alt="pokeballs" className="w-16 h-16" />
+        </div>
+        <div className="h-80 w-80 flex flex-col items-center justify-evenly">
+          {!registerOrLogin && (
+            <>
+              <button
+                onClick={() => setRegisterOrLogin("register")}
+                className="bg-mauve-300/90 hover:bg-mauve-400 transition-all duration-100 rounded-xl px-6 py-2 h-24 w-50 tracking-wider"
+              >
+                REGISTRARME
+              </button>
+              <button
+                className="bg-mauve-300/90 hover:bg-mauve-400 transition-all duration-100 rounded-xl px-6 py-2 h-24 w-50 tracking-wider"
+                onClick={() => setRegisterOrLogin("login")}
+              >
+                INICIAR SESION
+              </button>{" "}
+            </>
+          )}
+          {registerOrLogin === "register" ? (
+            <RegisterInput setRegisterOrLogin={setRegisterOrLogin} />
+          ) : registerOrLogin === "login" ? (
+            <LoginComponent setRegisterOrLogin={setRegisterOrLogin} />
+          ) : null}
+        </div>
       </div>
     </div>
   );
---
+++ front/src/components/RegisterComponent.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import axios from "axios";
+
+interface ComponentProps {
+  setRegisterOrLogin: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export default function RegisterComponent({
+  setRegisterOrLogin,
+}: ComponentProps) {
+  function handleRegister() {
+    const url = "http://localhost:5000/";
+    axios.get(url).then((response) => {
+      alert(`recibido con exito : ${response.data}`);
+    });
+  }
+
+  return (
+    <div className="h-80 flex flex-col items-center justify-evenly w-full px-4 relative">
+      <input
+        className="border border-mauve-400 rounded-xl p-2 text-md w-full"
+        type="email"
+        placeholder="Correo Electrónico"
+      />
+      <input
+        className="border border-mauve-400 rounded-xl p-2 text-md w-full"
+        type="text"
+        placeholder="Usuario"
+      />
+      <input
+        className="border border-mauve-400 rounded-xl p-2 text-md w-full"
+        type="password"
+        placeholder="Constraseña"
+      />
+      <button
+        className="bg-mauve-400 rounded-xl px-6 py-2 w-2/3"
+        onClick={() => handleRegister()}
+      >
+        Registrarme
+      </button>
+      <div className="text-xs text-mauve-600 flex items-center justify-end absolute bottom-0 right-2">
+        <button onClick={() => setRegisterOrLogin(null)}>◄ Volver</button>
+      </div>
+    </div>
+  );
+}
