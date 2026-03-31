ff0ad9d722565b27ed1c483d7851c92e7ab82246
demaime <aimedemian@gmail.com>
2026-03-31 11:31:38 -0300

    creado endpoint register, login a medias, modelo de prisma

--- back/index.ts
+++ back/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import cors from "cors";
+import { prisma } from "./prisma/lib/prisma";
+import type { User } from "./prisma/generated/prisma/client";
+
+//  import { User } from "./prisma/generated/prisma/client";
+// vi que puedo importar el tipo pero no me doy cuenta donde aplicarlo
+// creo que lo puse bien igual
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
@@ -13,7 +19,28 @@ app.listen(PORT, () =>
 
 app.get("/", (req, res) => res.send("Todo re bien"));
 
-app.post("/register", (req, res) => {
-  const { user, pass, email } = req.body;
-  res.json(req.body);
+app.post("/register", async (req, res) => {
+  const { email, user, password }: User = req.body;
+  const result = await prisma.user.create({
+    data: {
+      email,
+      user,
+      password,
+    },
+  });
+  res.json(result);
+});
+
+app.post("/login", async (req, res) => {
+  //quiero pasarle solo user y pass
+  // con findMany pude, con findUnique no
+  const { user, password }: User = req.body;
+  const result = await prisma.user.findMany({
+    //no me doy cuenta donde tipar. marca todo "any"
+    where: {
+      user: user,
+      password: password,
+    },
+  });
+  res.json(result);
 });
